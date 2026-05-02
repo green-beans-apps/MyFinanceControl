87dc14ba41885e8b420669924288bd62153c2901
Cristiano-woody <cristiano.nog.woody@gmail.com>
2026-05-01 21:38:46 -0300

    initial commit

---
+++ services/TelegramWebHook/src/main/java/com/green_beans_apps/MyFinanceControl/TelegramWebHook/TelegramWebHookApplication.java
@@ -0,0 +1,13 @@
+package com.green_beans_apps.MyFinanceControl.TelegramWebHook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWebHookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramWebHookApplication.class, args);
+	}
+
+}
---
+++ services/TelegramWebHook/src/test/java/com/green_beans_apps/MyFinanceControl/TelegramWebHook/TelegramWebHookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green_beans_apps.MyFinanceControl.TelegramWebHook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramWebHookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
